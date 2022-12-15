e47a6472d691f18367f9a146810048ce68a21c12
NhatPhan <nhat.phan@executionlab.asia>
2022-12-15 10:43:34 +0700

    update user model

--- src/controllers/User/index.ts
+++ src/controllers/User/index.ts
@@ -2,15 +2,23 @@ import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import moment from 'moment';
+import { floor, parseInt } from 'lodash';
 import User, { UserType } from '../../models/user';
 import { tokenGen, getIdFromReq, parseJwt } from '../../utils/token';
+import {
+  LoginRequest,
+  LogoutRequest,
+  RefreshTokenRequesst,
+  UpdateSelfUserRequest,
+  UpdateUserRequest,
+} from '../../models/api/user';
 
 let refreshTokens: string[] = [];
 
 const signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { displayName, email, username, password, birthday } =
-      req.body as UserType;
+    const { displayName, email, username, password, birthday }: UserType =
+      req.body;
     const findUser = await User.find({ email });
 
     if (findUser.length > 0) {
@@ -26,6 +34,8 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
         password: hashedPassword,
         birthday,
         info: undefined,
+        cart: [],
+        purchase: [],
       });
       const savedUser = await user.save();
       if (savedUser) {
@@ -48,7 +58,7 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { email, username, password } = req.body;
+    const { email, username, password }: LoginRequest = req.body;
     const findUser = username
       ? await User.find({ username })
       : await User.find({ email });
@@ -79,7 +89,7 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
 const logout = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { refreshToken } = req.body;
+    const { refreshToken }: LogoutRequest = req.body;
     if (refreshToken) {
       refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
     }
@@ -94,9 +104,72 @@ const getSelfUser = async (req: Request, res: Response, next: NextFunction) => {
     const _id = getIdFromReq(req);
     const user = await User.findById(_id);
     if (user) {
+      return res.status(200).json({
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        displayName: user.displayName,
+        birthday: user.birthday,
+        info: user.info,
+        role: user.role,
+      });
+    } else {
+      return res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+const getUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const _id = req.params.id;
+    const user = await User.findById(_id);
+    if (user) {
       return res.status(200).json(user);
     } else {
       return res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const _id = req.params.id;
+    const {
+      displayName,
+      username,
+      birthday,
+      info,
+      role,
+      email,
+      password,
+    }: UpdateUserRequest = req.body;
+    const findUser = await User.find({ username });
+    if (findUser.length > 0 && findUser[0]._id.toString() !== _id) {
+      return res.status(500).json({ message: 'Username already existed' });
+    } else {
+      const hashedPassword = password
+        ? await bcrypt.hash(password, 10)
+        : undefined;
+      const updatedUser = await User.findOneAndUpdate(
+        { _id },
+        {
+          $set: {
+            displayName,
+            username,
+            birthday,
+            info,
+            role,
+            email,
+            password: hashedPassword,
+          },
+        },
+        { new: true }
+      );
+      return res.status(200).json(updatedUser);
     }
   } catch (err) {
     return res.status(500).json({ message: err });
@@ -110,7 +183,8 @@ const updateSelfUser = async (
 ) => {
   try {
     const _id = getIdFromReq(req);
-    const { displayName, username, birthday, info } = req.body;
+    const { displayName, username, birthday, info }: UpdateSelfUserRequest =
+      req.body;
     const findUser = await User.find({ username });
     if (findUser.length > 0 && findUser[0]._id.toString() !== _id) {
       return res.status(500).json({ message: 'Username already existed' });
@@ -120,8 +194,43 @@ const updateSelfUser = async (
         { $set: { displayName, username, birthday, info } },
         { new: true }
       );
-      return res.status(200).json(updatedUser);
+      if (updatedUser) {
+        return res.status(200).json({
+          _id: updatedUser._id,
+          email: updatedUser.email,
+          username: updatedUser.username,
+          displayName: updatedUser.displayName,
+          birthday: updatedUser.birthday,
+          info: updatedUser.info,
+          role: updatedUser.role,
+        });
+      } else {
+        return res.status(500).json({ message: 'Update Profile Failed' });
+      }
     }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const _id = req.params.id;
+    const deletedUser = await User.deleteOne({ _id });
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(500).json({ messsage: err });
+  }
+};
+
+const getAllUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { offset, limit } = req.query;
+    const users = await User.find()
+      .skip(parseInt(offset?.toString() ?? '0'))
+      .limit(parseInt(limit?.toString() ?? '0'));
+    return res.status(200).json(users);
   } catch (err) {
     return res.status(500).json({ message: err });
   }
@@ -133,7 +242,7 @@ const refreshToken = async (
   next: NextFunction
 ) => {
   try {
-    const { refreshToken } = req.body;
+    const { refreshToken }: RefreshTokenRequesst = req.body;
     if (
       refreshToken &&
       refreshTokens.findIndex((token) => token === refreshToken) > -1
@@ -163,7 +272,11 @@ export default {
   login,
   logout,
   signup,
+  getUser,
   getSelfUser,
+  updateUser,
   updateSelfUser,
+  deleteUser,
+  getAllUser,
   refreshToken,
 };
